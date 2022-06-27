705258efc2a0bf8cc20ae46cdafd68727e7ec18a
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-06-27 15:14:34 +0800

    cool navbeans

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -6,16 +6,30 @@ import { NavItems } from "@/models/Content"
 
 export default function Navbar() {
   const router = useRouter()
+
+  const [sticky, setSticky] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () =>
+      window.scrollY > 100 ? setSticky(true) : setSticky(false)
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [])
+
   return (
-    <header>
-      <div className="wrapper-responsive flex justify-between">
+    <header
+      className={`fixed top-0 left-0 right-0 transition-all duration-300${
+        sticky !== true ? " py-[0.33rem]" : " py-0 bg-gray-400 bg-opacity-40"
+      }`}
+    >
+      <div className="wrapper-responsive flex justify-between select-none">
         <div>
           <Link href="/" passHref>
             logo
           </Link>
         </div>
         <nav>
-          <ul className="header-links flex gap-5">
+          <ul className="header-links flex gap-x-8 font-mouse-memoirs text-2xl">
             {NavItems.map((items, index) => (
               <li key={index}>
                 <Link href={items.route} passHref>
@@ -33,7 +47,13 @@ export default function Navbar() {
             ))}
             <li>
               <Link href="#" passHref>
-                <a className="border-2 border-orange-400 hover:bg-orange-400 py-[0.45rem] px-[1rem] rounded-lg text-orange-400 hover:text-white">
+                <a
+                  className={`border-2 py-[0.3rem] px-[1rem] rounded-lg hover:text-white ${
+                    sticky !== true
+                      ? "border-orange-400 hover:bg-orange-400 text-orange-400"
+                      : "border-orange-500 hover:bg-orange-500 text-orange-500"
+                  }`}
+                >
                   Buy
                 </a>
               </Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,41 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {}
+    fontFamily: {
+      "open-sans": ["Open Sans", ...defaultTheme.fontFamily.sans],
+      mono: ["Ubuntu Mono", ...defaultTheme.fontFamily.mono],
+      "mouse-memoirs": ["Mouse Memoirs", ...defaultTheme.fontFamily.sans]
+    },
+    extend: {
+      colors: {
+        blue: {
+          DEFAULT: "#1471EE",
+          50: "#DEEBFD",
+          100: "#CBE0FB",
+          200: "#9BC3F8",
+          300: "#70AAF5",
+          400: "#418DF1",
+          500: "#1471EE",
+          600: "#0E5ABE",
+          700: "#0A418A",
+          800: "#062956",
+          900: "#021021"
+        },
+        orange: {
+          DEFAULT: "#F1A125",
+          50: "#FDF2E2",
+          100: "#FCE8CA",
+          200: "#F9D7A4",
+          300: "#F6C479",
+          400: "#F4B14E",
+          500: "#F1A125",
+          600: "#CE810D",
+          700: "#955D0A",
+          800: "#5B3906",
+          900: "#261802"
+        }
+      }
+    }
   },
   plugins: []
 }
