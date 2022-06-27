967e45e4aa7cf06e7414f700c00b3ae5bf43dfaf
Kerby Keith Aquino <94678583+skepfusky@users.noreply.github.com>
2022-06-27 17:27:21 +0800

    css is pain

--- src/components/home/kickstarter.tsx
+++ src/components/home/kickstarter.tsx
@@ -1,3 +1,20 @@
+import styles from "./Kickstarter.module.scss"
+
 export default function Kickstarter() {
-  return <section>kickstarter</section>
+  return (
+    <section>
+      <div className="flex">
+        <div className={styles["book-container"]}>
+          {/* book-container */}
+          <div className={styles.book}>
+            {/* book */}
+            <div />
+          </div>
+        </div>
+        <article>
+          <h2>Yes on Kickstarter</h2>
+        </article>
+      </div>
+    </section>
+  )
 }
